35b8d19b16281580e85deff8c11c79e4147bac2d
李晨 <li.chen@trs.com.cn>
2018-12-28 15:25:03 +0800

    add Bidding

---
+++ Bidding/js/data/sidebar.js
@@ -0,0 +1,122 @@
+/**
+ * 左侧菜单 数据维护
+ * Li.chen
+ * 2018-11-20
+ */
+
+var global_sidebar = {
+    // 首页
+    'index': [
+        {
+            title: '上海保交所保险招投标平台' // 顶级菜单标识
+        },
+        {
+            active: 'PlatformIntroduction', // 导航菜单 标识
+            title: '招投标平台简介', // 菜单名称
+            infoFlag: 'ztbptjj-01', // 数据ID
+            link: ''
+        },
+        {
+            active: 'ScopeBusiness',
+            title: '招投标平台业务范围',
+            infoFlag: 'ztbptywfw-01',
+            link: ''
+        }
+    ],
+    // 业务规则与流程
+    'business': [
+        {
+            title: '上海保交所保险招投标平台'
+        },
+        {
+            active: 'BusinessRules',
+            title: '业务规则',
+            infoFlag: 'ywgz-01',
+            link: ''
+        },
+        {
+            active: 'OperationFlow',
+            title: '业务流程',
+            infoFlag: 'ywlc-01',
+            link: ''
+        }
+    ],
+    // 项目案例
+    'project': [
+        {
+            title: '上海保交所保险招投标平台'
+        },
+        {
+            active: 'ChildrenInsurance',
+            title: '“无忧计划-儿童保险礼物”',
+            infoFlag: 'etbxlw-01',
+            link: ''
+        },
+        {
+            active: 'LoveMid',
+            title: '“爱心中行”',
+            infoFlag: 'axxz-01',
+            link: ''
+        },
+        {
+            active: 'AuspiciousAnkang',
+            title: '“吉祥安康”',
+            infoFlag: 'jxak-01',
+            link: ''
+        },
+        {
+            active: 'GtjaSecurities',
+            title: '“国泰君安证券成长无忧计划”',
+            infoFlag: 'zqcz-01',
+            link: ''
+        },
+        {
+            active: 'FinancialInstitution',
+            title: '某商业保险项目',
+            infoFlag: 'jrjg-01',
+            link: ''
+        }
+    ],
+    // 新闻链接
+    'news': [
+        {
+            title: '上海保交所保险招投标平台'
+        },
+        {
+            active: 'ShieDynamic',
+            title: '保交所动态',
+            infoFlag: 'bjsdt-01',
+            link: ''
+        }
+    ],
+    // 公告
+    'announcement': [
+        {
+            title: '上海保交所保险招投标平台'
+        },
+        {
+            active: 'WinBidNotice',
+            title: '项目公告',
+            infoFlag: 'zbgg-01',
+            link: ''
+        },
+        {
+            active: 'RuleRevision',
+            title: '规则修订',
+            infoFlag: 'gzxd-01',
+            link: ''
+        }
+    ],
+    // 关于我们
+    'about': [
+        {
+            title: '上海保交所保险招投标平台'
+        },
+        {
+            active: 'contactus',
+            title: '联系我们',
+            infoFlag: 'lzwm-01',
+            link: ''
+        },
+    ]
+}
